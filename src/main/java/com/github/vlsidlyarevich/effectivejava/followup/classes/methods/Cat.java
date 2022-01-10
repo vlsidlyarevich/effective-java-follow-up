54e34479ad356b0a1743e92e8639e4e3a99d5fd0
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2022-01-10 19:26:18 +0300

    Classes and interfaces: attempt to fix links

---
+++ src/main/java/com/github/vlsidlyarevich/effectivejava/followup/classes/methods/Cat.java
@@ -0,0 +1,20 @@
+package com.github.vlsidlyarevich.effectivejava.followup.classes.methods;
+
+/**
+ * Cat
+ *
+ * @author Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
+ * Created on 1/10/22.
+ */
+public class Cat {
+
+    private final String name;
+
+    public Cat(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/github/vlsidlyarevich/effectivejava/followup/classes/methods/Main.java
@@ -0,0 +1,15 @@
+package com.github.vlsidlyarevich.effectivejava.followup.classes.methods;
+
+/**
+ * Main
+ *
+ * @author Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
+ * Created on 1/10/22.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Cat barsik = new Cat("barsik");
+        System.out.println(barsik.getName() + " said meow");
+    }
+}
